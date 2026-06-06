6ca5acafda50649b4e704e33624b5d965ceac18c
Codewithpabitra <pabitra05.maity@gmail.com>
2026-06-06 12:35:26 +0530

    add basic server setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,2 +1,25 @@
-console.log("Hello world");
-console.log("The server is running");
+import express, { type Request, type Response } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT ?? 5000;
+
+app.use(express.json());
+app.use(cors({
+    origin : process.env.CLIENT_URL || "http://localhost:5173",
+    credentials: true,
+}));
+
+app.use("/health", (req: Request, res: Response) => {
+    res.json({
+        status : "OK",
+        message : "FuseMeld API is Working"
+    })
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on PORT:${PORT}`);
+})
