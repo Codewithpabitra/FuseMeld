d119b9a109f5d412a33964a757010ccfcc20c33d
Codewithpabitra <pabitra05.maity@gmail.com>
2026-06-06 17:23:35 +0530

    add clerk provider in main.tsx file

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { ClerkProvider } from "@clerk/clerk-react";
 import './index.css'
 import App from './App.tsx'
 
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY as string;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("VITE_CLERK_PUBLISHABLE_KEY is missing from .env");
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
     <App />
+    </ClerkProvider>
   </StrictMode>,
 )
